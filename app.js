483a6d9e50a362b52e0a9a6cbd2bd49f0c2f6bd0
Izabelly Martins <izabellymartins820@gmail.com>
2024-04-05 01:19:17 -0300

    algumas alterações para deploy

--- app.js
+++ app.js
@@ -135,5 +135,5 @@ app.use('/admin', admin) // Com isso as rotas ganham o prefixo de ''admin''
 // Servidor
 const PORT = process.env.PORT || 8082
 app.listen(PORT, () => {
-    console.log('Servidor rodando!');
+    console.log('Servidor rodando na porta 8082!');
 });
